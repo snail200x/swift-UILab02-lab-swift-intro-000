4fa26f73ce3d4ca782678739dc0d473a48fa43a8
陈松毅 <chensongyi@ppzuche.com>
2016-12-04 21:32:44 +0800

    finish task

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -12,8 +12,40 @@ class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
     
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var btnColorSegmenter1: UISegmentedControl!
+    @IBOutlet weak var btnColorSegmenter2: UISegmentedControl!
+    
+    func getColorFromIndex(index:Int) -> String {
+        var color = "Blue"
+        switch index {
+        case 0:
+            color = "Red"
+        case 1:
+            color = "Yellow"
+        default:
+            color = "Blue"
+        }
+        return color
+    }
+    
+    func mixColor() {
+        let color1 = getColorFromIndex(index: btnColorSegmenter1.selectedSegmentIndex)
+        let color2 = getColorFromIndex(index: btnColorSegmenter2.selectedSegmentIndex)
+        print(color1,color2)
+        paintBucket.backgroundColor = mixColors(first:color1, second: color2)
+    }
+    
+    
+    @IBAction func btnColorSegmenter1(_ sender: UISegmentedControl) {
+        mixColor()
+    }
+    @IBAction func btnColorSegmenter2(_ sender: UISegmentedControl) {
+        mixColor()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        mixColor()
     }
 
     func mixColors(first: String, second: String) -> UIColor {
@@ -37,11 +69,4 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        // TODO: Implement this function according to the instructions.
-        
-        
-    }
-
 }
